f30fc287a37cb1c03c59091aaf0ffba261c582fa
joaopapereira <joaopapereira@gmail.com>
2012-07-06 14:34:58 +0000

    Initial import

---
+++ exampleProgram/exampleProgram1.cpp
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : exampleProgram.c
+ Author      : Joao Pereira
+ Version     :
+ Copyright   : This library is creating under the GPL license
+ Description : Uses shared library to print greeting
+               To run the resulting executable the LD_LIBRARY_PATH must be
+               set to ${project_loc}/libJPLogger/.libs
+               Alternatively, libtool creates a wrapper shell script in the
+               build directory of this program which can be used to run it.
+               Here the script will be called exampleProgram.
+ ============================================================================
+ */
+#define DEBUG 1
+#include "libJPLogger.h"
+
+// With this option will disable the debug to screen and do not allow any default log to be written
+
+int main(void) {
+  Logger log("/tmp/test.log");
+  log.log("Before first set","Ex1",M_LOG_LOW,M_LOG_INF);
+  log.setLogLvl("Ex1",M_LOG_MIN,M_LOG_INF);
+  log.log("Before change to normal","Ex1",M_LOG_LOW,M_LOG_INF);
+  log.setLogLvl("Ex1",M_LOG_NRM,M_LOG_INF);
+
+  log.log("This log will not appear","Ex1",M_LOG_LOW,M_LOG_INF);
+  log.log("But this one will","Ex1",M_LOG_HGH,M_LOG_INF);
+  log.log("And this one too","Ex1",M_LOG_NRM,M_LOG_INF);
+
+
+  OneInstanceLogger::instance()->log("Try to log with high[not show]","Ex1",M_LOG_HGH,M_LOG_INF);
+
+  OneInstanceLogger::instance()->copyLoggerDef( &log );
+  OneInstanceLogger::instance()->log("After the copy","Ex1",M_LOG_HGH,M_LOG_INF);
+  OneInstanceLogger::instance()->setLogLvl("Ex1",M_LOG_NRM,M_LOG_INF);
+  OneInstanceLogger::instance()->log("After copied change","Ex1",M_LOG_LOW,M_LOG_INF);
+
+  log.setLogLvl("Ex1Set1",M_LOG_MIN,M_LOG_WRN);
+  log.log("I will appear","Ex1Set1",M_LOG_LOW,M_LOG_WRN);
+  OneInstanceLogger::instance()->log("But i will not","Ex1Set1",M_LOG_LOW,M_LOG_WRN);
+
+
+  return 0;
+}
---
+++ libJPLogger/libJPLogger.cpp
@@ -0,0 +1,306 @@
+#include "libJPLogger.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <string.h>
+
+using namespace std;
+
+
+template <typename K, typename V, class C, class A>
+std::ostream &operator<< (std::ostream &os, std::map<K,V,C,A> const& m)
+{
+	os << "{ ";
+	typename std::map<K,V,C,A>::const_iterator p;
+	for (p = m.begin(); p != m.end(); ++p) {
+		os << p->first << ":" << p->second << ", ";
+	}
+	return os << "}";
+}
+
+LoggerExpFileError::LoggerExpFileError(char* error, bool showErrno ) throw(){
+	setMsg(error);
+	this->showErrno = showErrno;
+}
+LoggerExpFileError::LoggerExpFileError(std::string error, bool showErrno ) throw(){
+	setMsg(error.c_str());
+	this->showErrno = showErrno;
+}
+LoggerExpFileError::LoggerExpFileError( bool showErrno ) throw(){
+	setMsg("Error happened on logger file");
+	this->showErrno = showErrno;
+}
+void
+LoggerExpFileError::setMsg(const char * error) throw(){
+	myerror = error;
+	if( showErrno && errno != 0 ){
+		myerror += "[" ;
+		myerror += strerror(errno);
+		myerror += "]";
+	}
+}
+LoggerExpFileError::~LoggerExpFileError() throw(){
+}
+
+const char *
+LoggerExpFileError::what() const throw(){
+	return myerror.c_str();
+}
+
+
+
+
+Logger::Logger( std::string filename )
+{
+	load();
+	if( 0 != setFile( filename ) ){
+		return;
+	}
+}
+Logger::Logger()
+{
+	load();
+}
+Logger::Logger(Logger * logger)
+{
+	load();
+	copyLoggerDef( logger );
+}
+
+void
+Logger::load(){
+	CONST_DEFMODULE = "ALL";
+#ifndef DEBUG
+	setLogLvl( CONST_DEFMODULE, M_LOG_MIN, M_LOG_ALLLVL );
+#else
+	setLogLvl( CONST_DEFMODULE, M_LOG_MAX, M_LOG_ALLLVL );
+#endif
+
+	M_LOG_TRANSLATE[M_LOG_TRC] = "TRC";
+	M_LOG_TRANSLATE[M_LOG_DBG] = "DBG";
+	M_LOG_TRANSLATE[M_LOG_INF] = "INF";
+	M_LOG_TRANSLATE[M_LOG_WRN] = "WRN";
+	M_LOG_TRANSLATE[M_LOG_ERR] = "ERR";
+
+}
+
+Logger::~Logger(){
+	myfile.close();
+}
+
+int
+Logger::setFile(std::string filename ){
+	myfile.open( filename.c_str() , ios::app);
+	if( !myfile.is_open() ){
+		cerr << "Log file:[" << filename <<
+			"] could not be opened" << endl;
+		throw LoggerExpFileError(true);
+	}
+	outputFile = filename;
+	return 0;
+}
+/**
+ * Retrive the file to write the log to
+ * @return the file path and name
+ */
+std::string
+Logger::getFile(){
+	return outputFile;
+}
+
+
+int
+Logger::setLogLvl( std::string module , int logsev, int type)
+{
+	LogModules::iterator it;
+	LogType aux;
+	int actType, actlogsev;
+	pair<LogType::iterator,bool> ret;
+	if( type >= M_LOG_LASTTYPE )
+		actType = M_LOG_LASTTYPE - 1;
+	else if( type <= M_LOG_NULLTYPE)
+		actType = M_LOG_NULLTYPE + 1;
+	else
+		actType = type;
+	if( logsev >= M_LOG_MAX )
+		actlogsev = M_LOG_MAX - 1;
+	else if( logsev <= M_LOG_NULL )
+		actlogsev = M_LOG_NULL + 1;
+	else
+		actlogsev = logsev;
+
+	aux.insert( pair<int,int>(actType,actlogsev));
+
+	it = Logger::logLvls.find( module );
+
+	if(  Logger::logLvls.end() == it  ){
+		Logger::logLvls.insert( pair<std::string,LogType> (module,aux ) );
+	}else{
+		ret = Logger::logLvls[module].insert(make_pair ( actType , actlogsev ));
+		if( false == ret.second )
+		{
+			 Logger::logLvls[module][actType] = actlogsev;
+		}
+	}
+
+	cout << logLvls << endl;
+
+	return 0;
+}
+int
+Logger::unsetModule( std::string module ){
+	Logger::logLvls.erase( module );
+	return 0;
+}
+
+/**
+ * Retrieve the log levels and modules of a logger
+ * @return Type
+ */
+const LogModules
+Logger::getLogLvls(){
+	return (const LogModules )Logger::logLvls;
+}
+
+
+void Logger::log( std::string message , std::string module , int logsev, int type)
+{
+	LogModules::iterator it;
+	LogType::iterator it1;
+
+	try{
+		if( writable(module , logsev, type ) )
+			write( message , module , type );
+	}catch( LoggerExpFileError &e ){
+		cerr << e.what();
+	}
+}
+void Logger::log( std::string module , int logsev, int type, std::string message ,...)
+{
+	va_list args;
+	char outMsg[5000];
+	std::string toWrite;
+	va_start( args, message.c_str() );
+	vsprintf( outMsg , message.c_str() , args );
+	va_end( args );
+	message = outMsg;
+	try{
+		if( writable(module , logsev, type ) )
+			write( message , module , type );
+	}catch( LoggerExpFileError &e ){
+			cerr << e.what();
+	}
+}
+
+bool Logger::writable( std::string module , int logsev, int type )
+{
+	LogModules::iterator it;
+	LogType::iterator it1,itl1;
+
+	bool writable = true;
+	int actType;
+	it = logLvls.find(module);
+
+	if( type > M_LOG_LASTTYPE )
+		actType = M_LOG_LASTTYPE - 1;
+	else if( type <= M_LOG_NULLTYPE )
+		actType = M_LOG_NULLTYPE + 1;
+	else
+		actType = type;
+
+	//No specific configuration for the module
+	// Assumption: M_LOG_ALL_LVL is always set'ed with the log level by default
+	if( logLvls.end() == it ){
+		cout << "No cfg for module" << endl;
+		if(  logLvls[CONST_DEFMODULE][M_LOG_ALLLVL] < logsev )
+			writable = false;
+	}else{
+		it1 = logLvls[module].find(actType);
+		//There is no configuration for the log level
+		if( logLvls[module].end() == it1 )
+		{
+			cout << "not cfg for log level" << endl;
+			itl1 = logLvls[module].find(M_LOG_ALLLVL);
+			// No configuration in this module for ALL levels
+			if( logLvls[module].end() == itl1 ){
+				//Assumption M_LOG_ALLLVL is always set'ed with the log level by default
+				if( logLvls[CONST_DEFMODULE][M_LOG_ALLLVL] > logsev )
+						writable = false;
+			// Configuration for all levels found
+			}else if( logLvls[module][M_LOG_ALLLVL] > logsev )
+					writable = false;
+		}else//There is a configuration for the log level
+		{
+			cout << logLvls[module][actType] << ">" <<logsev<< endl;
+			if( logLvls[module][actType] > logsev )
+			{
+				cout << "actlvl < logsev" << endl;
+				writable = false;
+			}
+		}
+
+	}
+	cout << (writable?"writing":"Not writing") << endl;
+	return writable;
+}
+int Logger::write( std::string message, std::string module , int type ){
+
+	debugFun( "writing:[" << module << "][" << type <<  "]" << message<<endl);
+	char dateResult[20];
+	{
+		struct tm *tmp;
+		time_t t = time(NULL);
+
+		tmp = localtime(&t);
+		if (strftime(dateResult, sizeof(dateResult), "%Y-%m-%d %H:%M:%S", tmp) == 0) {
+			throw LoggerExpFileError("Error writing log",true);
+		}
+	}
+	myfile << dateResult << " "<< module << "[" << M_LOG_TRANSLATE[type] << "]" <<"\t"
+			<< message << endl;
+	myfile.flush();
+
+
+	return 0;
+}
+/**
+ * Set logger level
+ * @param lvls Log module levels
+ * @return Return 0 in case of success
+ */
+int
+Logger::setLoggerLevel( const LogModules lvls){
+	Logger::logLvls.clear();
+	Logger::logLvls = (LogModules)lvls;
+}
+
+/**
+ * Copy the definitions of a logger
+ * @param logger Logger to copy definitions from
+ * @return Return 0 in case of success
+ */
+int
+Logger::copyLoggerDef( Logger * logger ){
+	cout << "copyLoggerDef"<<endl;
+	try{
+		setFile(logger->getFile());
+	}catch( LoggerExpFileError &e ){
+		cerr << e.what()<< endl;
+	}
+	cout << "going out"<<endl;
+	return setLoggerLevel( logger->getLogLvls() );
+
+}
+
+OneInstanceLogger* OneInstanceLogger::inst = NULL;
+OneInstanceLogger *
+OneInstanceLogger::instance(){
+	if (!inst )
+		inst = new OneInstanceLogger();
+	return inst;
+};
+
+OneInstanceLogger::OneInstanceLogger()
+:Logger(){
+
+}
